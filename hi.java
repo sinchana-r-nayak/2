a88fac89340ccb75f4a245ff624503ca59eb6193
sinchana-r-nayak <156403118+sinchana-r-nayak@users.noreply.github.com>
2025-03-20 12:48:25 +0530

    Create hi.java

---
+++ hi.java
@@ -0,0 +1,13 @@
+public class hi {
+    public static void main(String[] args) {
+        // Define two numbers directly in the code
+        double num1 = 5.5; // You can change this value
+        double num2 = 3.2; // You can change this value
+        
+        // Add the numbers
+        double sum = num1 + num2;
+        
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
